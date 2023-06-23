05c46e5f9af440b4ce2db407c7378a32431c9fe2
Javier <l1830090992@villahermosa.tecnm.mx>
2023-06-23 08:32:58 -0600

    busqueda

--- app/Http/Livewire/MostrarRegistros.php
+++ app/Http/Livewire/MostrarRegistros.php
@@ -7,9 +7,32 @@ use Livewire\Component;
 
 class MostrarRegistros extends Component
 {
+    public $expediente;
+    public $termino;
+    public $dependencia;
+    protected $listeners = ['terminosBusqueda' => 'buscar'];
+    public function buscar($termino, $expediente, $dependencia)
+    {
+        $this->termino = $termino;
+        $this->dependencia = $dependencia;
+        $this->expediente = $expediente;
+    }
     public function render()
     {
-        $registros = Registro::where('reg_fkusuario', auth()->user()->id)->paginate(10);
+        $registros = registro::when($this->termino, function ($query) {
+            $query->where('reg_asunto', 'LIKE', "%" . $this->termino . "%");
+        })
+            ->paginate();
+        $registros = Registro::where('reg_fkusuario', auth()->user()->id)->when($this->termino, function ($query) {
+            $query->where('reg_asunto', 'LIKE', "%" . $this->termino . "%");
+        })->when($this->termino, function ($query) {
+            $query->orWhere('reg_ndocumento', 'LIKE', "%" . $this->termino . "%");
+        })->when($this->dependencia, function ($query) {
+            $query->where('reg_fkdepedencia', $this->dependencia);
+        })->when($this->expediente, function ($query) {
+            $query->where('reg_fkexpediente', $this->expediente);
+        })
+            ->paginate();
         return view('livewire.mostrar-registros', [
             'registros' => $registros,
         ]);
